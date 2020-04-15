57b6c1c17659248121106a27b594c8a151a32fea
bigmak40 <michael.kuck+github@gmail.com>
2020-04-15 14:47:00 -0400

    Adding service check
    Still need Boolean for bad and message

--- PingMonitor/PingMonitor.ps1
+++ PingMonitor/PingMonitor.ps1
@@ -4,24 +4,25 @@ $chatID  = ${{ secrets.MY_TELEGRAM_CHAT_ID }}
 
 # What is being monitored
 $monitorList = @(
-                  ("192.168.1.1",   "Ubiquiti USG Router"),
-                  ("192.168.1.2",   "Ubiquiti UAP-AC-Pro (Upstairs)"),
-                  ("192.168.1.3",   "Ubiquiti UAP-AC-Pro (Downstairs)"),
-                  ("192.168.1.4",   "Ubiquiti UAP-AC-M (Front Yard)"),
-                  ("192.168.1.6",   "Ubiquiti Cloud Key"),
-                  ("192.168.1.7",   "Ubiquiti USL-24P (Core Switch)"),
-                  ("192.168.1.8",   "Ubiquiti US-8-150W (Garage Attic)"),
-                 #("192.168.1.9",   "Ubiquiti US-8-60W (Family Room)"),
-                  ("192.168.1.112", "Pihole Primary (Dell T30)"),
-                  ("192.168.4.7",   "UVC G3 Micro 1 (Nursery)"),
-                 #("192.168.4.8",   "UVC G3 Flex 1 (Family Room)"),
-                  ("192.168.4.9",   "UVC G3 Flex 2 (Kitchen)"),
-                  ("192.168.4.10",  "UVC G3 Pro 1 (Garage Inside)"),
-                  ("192.168.4.12",  "Dahua Turret 1 (Garage Center)"),
-                  ("192.168.4.13",  "Dahua PTZ 1 (Garage Right)"),
-                  ("192.168.4.14",  "Dahua Twin 1 (Garage Left)"),
-                  ("192.168.4.15",  "Dahua PTZ 2 (Garage Left)"),
-                  ("192.168.4.16",  "Dahua Turret 2 (TBD Location)")
+                  ("IP",  "192.168.1.1",   "Ubiquiti USG Router"),
+                  ("IP",  "192.168.1.2",   "Ubiquiti UAP-AC-Pro (Upstairs)"),
+                  ("IP",  "192.168.1.3",   "Ubiquiti UAP-AC-Pro (Downstairs)"),
+                  ("IP",  "192.168.1.4",   "Ubiquiti UAP-AC-M (Front Yard)"),
+                  ("IP",  "192.168.1.6",   "Ubiquiti Cloud Key"),
+                  ("IP",  "192.168.1.7",   "Ubiquiti USL-24P (Core Switch)"),
+                  ("IP",  "192.168.1.8",   "Ubiquiti US-8-150W (Garage Attic)"),
+                 #("IP",  "192.168.1.9",   "Ubiquiti US-8-60W (Family Room)"),
+                  ("IP",  "192.168.1.112", "Pihole Primary (Dell T30)"),
+                  ("IP",  "192.168.4.7",   "UVC G3 Micro 1 (Nursery)"),
+                 #("IP",  "192.168.4.8",   "UVC G3 Flex 1 (Family Room)"),
+                  ("IP",  "192.168.4.9",   "UVC G3 Flex 2 (Kitchen)"),
+                  ("IP",  "192.168.4.10",  "UVC G3 Pro 1 (Garage Inside)"),
+                  ("IP",  "192.168.4.12",  "Dahua Turret 1 (Garage Center)"),
+                  ("IP",  "192.168.4.13",  "Dahua PTZ 1 (Garage Right)"),
+                  ("IP",  "192.168.4.14",  "Dahua Twin 1 (Garage Left)"),
+                  ("IP",  "192.168.4.15",  "Dahua PTZ 2 (Garage Left)"),
+                  ("IP",  "192.168.4.16",  "Dahua Turret 2 (TBD Location)"),
+                  ("SVC", "blueiris",      "Blue Iris Service")
                 )
 
 # How often is it being monitored (seconds)
@@ -39,22 +40,40 @@ $repeat = 1
 # PingMonitor
 while($repeat){
     foreach($monitoring in $monitorList){
-        $thisIP   = $monitoring[0]
-        $thisName = $monitoring[1]
-        if(!(Test-Connection -ComputerName $thisIP -BufferSize 16 -Count 1 -ea 0 -quiet)){
-            Start-Sleep -Seconds $recheckInterval
-            if(!(Test-Connection -ComputerName $thisIP -BufferSize 16 -Count 1 -ea 0 -quiet)){
-                $now = Get-Date -Format "M/d/yy hh:mm:ss"
-                $Message = "At $($now): Ping error on $($thisIP) [$($thisName)]"
-                Write-Output $Message
-                try   { Invoke-RestMethod -Uri "https://api.telegram.org/bot$($MyToken)/sendMessage?chat_id=$($chatID)&text=$($Message)" } 
-                catch {
-                        Start-Sleep -Seconds $resendInterval
-                        try   { Invoke-RestMethod -Uri "https://api.telegram.org/bot$($MyToken)/sendMessage?chat_id=$($chatID)&text=$($Message)" }
-                        catch { "Well that's a failure..." }
-                }
-            }
-        }
+        $thisType = $monitoring[0]
+        $thisID   = $monitoring[1]
+        $thisName = $monitoring[2]
+
+        # see if ip
+        if($thisType -eq "IP"){
+            if(!(Test-Connection -ComputerName $thisID -BufferSize 16 -Count 1 -ea 0 -quiet)){
+               Start-Sleep -Seconds $recheckInterval
+               if(!(Test-Connection -ComputerName $thisID -BufferSize 16 -Count 1 -ea 0 -quiet)){
+                   $now = Get-Date -Format "M/d/yy hh:mm:ss"
+                   $Message = "At $($now): Ping error on $($thisID) [$($thisName)]"
+                   Write-Output $Message
+                   try   { Invoke-RestMethod -Uri "https://api.telegram.org/bot$($MyToken)/sendMessage?chat_id=$($chatID)&text=$($Message)" } 
+                   catch {
+                           Start-Sleep -Seconds $resendInterval
+                           try   { Invoke-RestMethod -Uri "https://api.telegram.org/bot$($MyToken)/sendMessage?chat_id=$($chatID)&text=$($Message)" }
+                           catch { "Well that's a failure..." }
+                   }
+               }
+          }
+         # see if service
+         if($thisType -eq "SVC" ){
+             if(){
+                 $now = Get-Date -Format "M/d/yy hh:mm:ss"
+                 $Message = "At $($now): Ping error on $($thisID) [$($thisName)]"
+                 try   { Invoke-RestMethod -Uri "https://api.telegram.org/bot$($MyToken)/sendMessage?chat_id=$($chatID)&text=$($Message)" } 
+                 catch {
+                         Start-Sleep -Seconds $resendInterval
+                         try   { Invoke-RestMethod -Uri "https://api.telegram.org/bot$($MyToken)/sendMessage?chat_id=$($chatID)&text=$($Message)" }
+                         catch { "Well that's a failure..." }
+                       }
+                 }
+              }
+          }
     }
     Start-Sleep -Seconds $setInterval
 }
