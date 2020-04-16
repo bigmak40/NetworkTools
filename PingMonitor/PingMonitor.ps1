037d558becfa47ddf103c433d5bc4c33bde1e97a
bigmak40 <michael.kuck+github@gmail.com>
2020-04-15 20:24:22 -0400

    Update PingMonitor.ps1

--- PingMonitor/PingMonitor.ps1
+++ PingMonitor/PingMonitor.ps1
@@ -11,31 +11,31 @@ $recheckInterval = 10 # If it fails on try #1, how long to wait until try #2 (se
 #################################################
 # Monitoring list
 $monitorList = @(
-		("IP",  "192.168.1.1",   "Ubiquiti USG Router"),
-                ("IP",  "192.168.1.2",   "Ubiquiti UAP-AC-Pro (Upstairs)"),
-                ("IP",  "192.168.1.3",   "Ubiquiti UAP-AC-Pro (Downstairs)"),
-                ("IP",  "192.168.1.4",   "Ubiquiti UAP-AC-M (Front Yard)"),
-                ("IP",  "192.168.1.6",   "Ubiquiti Cloud Key"),
-                ("IP",  "192.168.1.7",   "Ubiquiti USL-24P (Core Switch)"),
-                ("IP",  "192.168.1.8",   "Ubiquiti US-8-150W (Garage Attic)"),
-               #("IP",  "192.168.1.9",   "Ubiquiti US-8-60W (Family Room)"),
-                ("IP",  "192.168.1.112", "Pihole Primary (Dell T30)"),
-                ("IP",  "192.168.4.7",   "UVC G3 Micro 1 (Nursery)"),
-               #("IP",  "192.168.4.8",   "UVC G3 Flex 1 (Family Room)"),
-                ("IP",  "192.168.4.9",   "UVC G3 Flex 2 (Kitchen)"),
-	        ("IP",  "192.168.4.10",  "UVC G3 Pro 1 (Garage Inside)"),
-                ("IP",  "192.168.4.12",  "Dahua Turret 1 (Garage Center)"),
-                ("IP",  "192.168.4.13",  "Dahua PTZ 1 (Garage Right)"),
-                ("IP",  "192.168.4.14",  "Dahua Twin 1 (Garage Left)"),
-                ("IP",  "192.168.4.15",  "Dahua PTZ 2 (Garage Left)"),
-                ("IP",  "192.168.4.16",  "Dahua Turret 2 (TBD Location)"),
-                ("SVC", "blueiris",      "Blue Iris Service")
-                )
+    ("IP",  "192.168.1.1",   "Ubiquiti USG Router"),
+    ("IP",  "192.168.1.2",   "Ubiquiti UAP-AC-Pro (Upstairs)"),
+    ("IP",  "192.168.1.3",   "Ubiquiti UAP-AC-Pro (Downstairs)"),
+    ("IP",  "192.168.1.4",   "Ubiquiti UAP-AC-M (Front Yard)"),
+    ("IP",  "192.168.1.6",   "Ubiquiti Cloud Key"),
+    ("IP",  "192.168.1.7",   "Ubiquiti USL-24P (Core Switch)"),
+    ("IP",  "192.168.1.8",   "Ubiquiti US-8-150W (Garage Attic)"),
+   #("IP",  "192.168.1.9",   "Ubiquiti US-8-60W (Family Room)"),
+    ("IP",  "192.168.1.112", "Pihole Primary (Dell T30)"),
+    ("IP",  "192.168.4.7",   "UVC G3 Micro 1 (Nursery)"),
+   #("IP",  "192.168.4.8",   "UVC G3 Flex 1 (Family Room)"),
+    ("IP",  "192.168.4.9",   "UVC G3 Flex 2 (Kitchen)"),
+    ("IP",  "192.168.4.10",  "UVC G3 Pro 1 (Garage Inside)"),
+    ("IP",  "192.168.4.12",  "Dahua Turret 1 (Garage Center)"),
+    ("IP",  "192.168.4.13",  "Dahua PTZ 1 (Garage Right)"),
+    ("IP",  "192.168.4.14",  "Dahua Twin 1 (Garage Left)"),
+    ("IP",  "192.168.4.15",  "Dahua PTZ 2 (Garage Left)"),
+    ("IP",  "192.168.4.16",  "Dahua Turret 2 (TBD Location)"),
+    ("SVC", "blueiris",      "Blue Iris Service")
+)
 #################################################
 # SendTelegramMessage function
 function SendTelegramMessage($Message){
     try   { Invoke-RestMethod -Uri "https://api.telegram.org/bot$($MyToken)/sendMessage?chat_id=$($chatID)&text=$($Message)" | Out-Null} 
-	catch { Start-Sleep -Seconds $resendInterval
+    catch { Start-Sleep -Seconds $resendInterval
             try   { Invoke-RestMethod -Uri "https://api.telegram.org/bot$($MyToken)/sendMessage?chat_id=$($chatID)&text=$($Message)" | Out-Null}
             catch { "Well that's a failure..." }
           }
@@ -43,7 +43,7 @@ function SendTelegramMessage($Message){
 #################################################
 # PingMonitor body
 while(1){
-	foreach($monitoring in $monitorList){
+    foreach($monitoring in $monitorList){
         $thisType = $monitoring[0]
         $thisID   = $monitoring[1]
         $thisName = $monitoring[2]
@@ -55,7 +55,7 @@ while(1){
                 if(!(Test-Connection -ComputerName $thisID -BufferSize 16 -Count 1 -ea 0 -quiet)){
                     $now = Get-Date -Format "M/d/yy HH:mm:ss"
                     $Message = "At $($now): Ping error on $($thisID) [$($thisName)]"
-					SendTelegramMessage($Message)
+                    SendTelegramMessage($Message)
                 }
             }
         }
@@ -66,10 +66,10 @@ while(1){
                 $now = Get-Date -Format "M/d/yy HH:mm:ss"
                 $Message = "At $($now): Service stopped $($thisID) [$($thisName)]"
                 SendTelegramMessage($Message)
-			}
+            }
         }
     }
-	
+
     # Stop running if this is set to not repeat
     if(!$repeat){break}
 
